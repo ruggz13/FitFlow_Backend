e495427c0a683c22e516d1bacd6454e7f90127d8
Trevahr Robertson <Trevahr@gmail.com>
2019-08-19 12:20:43 -0400

    better api response

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,12 +9,40 @@ class UsersController < ApplicationController
 
     def login
         user = User.find_by(username: params[:username])
-        session[:id] = user.id
-        render json: {user: user, user_workouts: user.user_workouts, workouts: user.workouts}
+        # user_workouts = UserWorkout.find_by(user_id: user.id)
+
+        render json: user.to_json(user_serializer)
     end
 
     def logout
         session[:id] = nil
+    end
+
+
+    private
+
+    def user_serializer
+        {
+            :only => [:id, :name, :username],
+            :include => {:user_workouts => 
+                {
+                    :only => [:day_id], 
+                    :include => {
+                        :workout => {
+                        :only => [:name, :video_url,]
+                        }
+                    }
+                }
+            }
+        }
+        # {
+        #     id: self.id,
+        #     name: self.name,
+        #     username: self.username,
+        #     user_workouts: self.user_workouts [
+        #         {self == user_workout}
+        #     ]
+        # }
     end
 
 
