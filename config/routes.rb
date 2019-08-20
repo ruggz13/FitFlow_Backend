9ada4d6168084a87948e9cc8d3baf646936f7dc6
Trevahr Robertson <Trevahr@gmail.com>
2019-08-20 08:59:51 -0400

    working delete buttons and cleared fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :user_workouts
-  resources :workouts
   get '/login/:username', to: 'users#login'
   post '/addworkout/:id', to: 'users#addworkouts'
+  delete '/user_workouts/:username', to: 'users#delete_workout'
+  resources :workouts
+  resources :user_workouts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_142248) do
+ActiveRecord::Schema.define(version: 2019_08_20_125718) do
 
   create_table "user_workouts", force: :cascade do |t|
     t.integer "user_id"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_08_16_142248) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
