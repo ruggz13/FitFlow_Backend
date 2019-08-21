103f384abee81708099c943dfb18877f5fbb0601
ruggz13 <nicolasruggieri7@gmail.com>
2019-08-21 09:01:53 -0400

    changed backend

--- app/controllers/user_workouts_controller.rb
+++ app/controllers/user_workouts_controller.rb
@@ -5,10 +5,6 @@ class UserWorkoutsController < ApplicationController
     render json: user_workouts
     end
 
-
-
-
-
     def show
         user = User.find(params[:id])
         # byebug
@@ -17,6 +13,15 @@ class UserWorkoutsController < ApplicationController
         # bird: sighting.bird, location: sighting.location }
     end
 
+    def create
+        user_workout = UserWorkout.create(user_workouts_params)
+        byebug
+    end
 
+    private
+
+    def user_workouts_params
+        params.require(:user_workout).permit(:user_id, :workout_id)
+    end
 
 end
